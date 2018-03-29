86423ca583e22637e2bd020573f5ade7a575b0eb
BY <beyearn@gmail.com>
2018-03-29 18:33:20 +0800

    Initial commit

---
+++ src/main/java/ssm/blog/dao/BloggerDao.java
@@ -0,0 +1,18 @@
+package ssm.blog.dao;
+
+import org.springframework.stereotype.Repository;
+import ssm.blog.entity.Blogger;
+
+/**
+ * Created by xp on 2017/4/13.
+ * 博主dao接口
+ */
+@Repository //注册为持久层的bean
+public interface BloggerDao {  //就是xxxxmapper
+    /**
+     * 查询博主信息
+     *
+     * @return
+     */
+    Blogger getBloggerData();
+}
